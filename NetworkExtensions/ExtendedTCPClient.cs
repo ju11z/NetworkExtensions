2b5bfe155333560c458df672d48641e6388678f3
ju11z <fgtmnt@mail.ru>
2024-10-06 14:23:36 +0300

    added async read and write methods;

--- NetworkExtensions/ExtendedTCPClient.cs
+++ NetworkExtensions/ExtendedTCPClient.cs
@@ -61,6 +61,21 @@ namespace TCPClientExtensions
             nwStream.Write(messageBytes, 0, messageBytes.Length);
         }
 
+        public static async void WriteCustomAsync(this TcpClient client, string message)
+        {
+            ulong messageSize = (ulong)message.Length;
+
+            byte[] messageSizeBytes = BitConverter.GetBytes(messageSize);
+            byte[] messageBytes = ASCIIEncoding.ASCII.GetBytes(message);
+
+            Console.WriteLine($"Sending {messageSize} bytes. Message: {message}");
+
+            NetworkStream nwStream = client.GetStream();
+
+            await nwStream.WriteAsync(messageSizeBytes, 0, messageSizeBytes.Length);
+            await nwStream.WriteAsync(messageBytes, 0, messageBytes.Length);
+        }
+
         public static string ReadCustom(this TcpClient client)
         {
             NetworkStream nwStream = client.GetStream();
@@ -71,6 +86,27 @@ namespace TCPClientExtensions
 
             byte[] receivedMessageBytes = new byte[dataReceivedSize];
             nwStream.Read(receivedMessageBytes, 0, receivedMessageBytes.Length);
+
+            string dataReceived = Encoding.ASCII.GetString(receivedMessageBytes);
+
+            if (dataReceivedSize != (ulong)dataReceived.Length)
+                throw new CorruptedDataException($"Not all data was recived correctly. Received message: {dataReceived}. Expected to receive {dataReceivedSize} bytes, but got {dataReceived.Length} bytes instead.");
+
+            Console.WriteLine($"Receiving {dataReceivedSize} bytes. Message: {dataReceived}");
+
+            return dataReceived;
+        }
+
+        public static async Task<string> ReadCustomAsync(this TcpClient client)
+        {
+            NetworkStream nwStream = client.GetStream();
+
+            byte[] receivedMessageSizeBytes = new byte[8];
+            await nwStream.ReadAsync(receivedMessageSizeBytes, 0, receivedMessageSizeBytes.Count());
+            ulong dataReceivedSize = BitConverter.ToUInt64(receivedMessageSizeBytes, 0);
+
+            byte[] receivedMessageBytes = new byte[dataReceivedSize];
+            await nwStream.ReadAsync(receivedMessageBytes, 0, receivedMessageBytes.Length);
 
             string dataReceived = Encoding.ASCII.GetString(receivedMessageBytes);
 
